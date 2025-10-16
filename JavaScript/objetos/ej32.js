f89f24ac280eac34e135342b03cb351083d90468
solvam <alumno@solvam.local>
2025-10-16 13:51:22 +0200

    16-10-solvam

---
+++ JavaScript/objetos/ej32.js
@@ -0,0 +1,39 @@
+let users = []
+let even = []
+while(true){
+    let conf = confirm("Do you want add other user?")
+    if (conf){
+        let user = {
+            name: prompt("Enter a name: "),
+            surname: prompt("Enter a surname: "),
+            age: prompt("Enter an age: ")
+        }
+        if (!isNaN(user["age"]) && user["age"] >= 0){
+            users.push(user);
+        } else {
+            alert("Age not valid!!! Joselu dejame ver a los niños")
+        }
+    } else {
+        break;
+    }
+}
+console.log(users);
+
+function u21(x){
+    return x.age <= 21;
+}
+function ageEven(x){
+    if (x.age % 2 == 0){
+        even.push(x.name + " " + x.surname + " " + x.age + " ");
+    }
+}
+
+let under21 = users.every(u21);
+
+if (under21){
+    users.filter(ageEven);
+} else {
+    alert("Age >21 Joselu cariño te quiero, volvamos juntos")
+}
+
+document.writeln(even.join(", "))
---
+++ JavaScript/objetos/ej33.js
@@ -0,0 +1,54 @@
+let vinylCollection = [
+  {
+    "id": 1,
+    "album_title": "El Fary es la Ley",
+    "artist": "El Fary",
+    "tracklist": [
+      "El Toro Guapo",
+      "La Mandanga",
+      "Voy a Pintar La Carpa"
+    ]
+  },
+  {
+    "id": 2,
+    "album_title": "Estopa",
+    "artist": "Estopa",
+    "tracklist": [
+      "La Raja de Tu Falda",
+      "Tu Calorro",
+      "Vino Tinto"
+    ]
+  },
+  {
+    "id": 3,
+    "album_title": "Mientras No Cueste Trabajo",
+    "artist": "Melendi",
+    "tracklist": [
+      "Con La Luna Llena",
+      "El Nano",
+      "Por Amarte Tanto"
+    ]
+  },
+  {
+    "id": 4,
+    "album_title": "No Pares de Soñar",
+    "artist": "La Húngara",
+    "tracklist": [
+      "Vete De Mi Vida",
+      "Borracho De Amor",
+      "Maldita Suerte"
+    ]
+  },
+  {
+    "id": 5,
+    "album_title": "Fiesta Española",
+    "artist": "Manolo Escobar",
+    "tracklist": [
+      "El Porompompero",
+      "Mi Carro",
+      "Ay Jaleo"
+    ]
+  }
+]
+
+let findId = parseInt(prompt("Enter an Id: "));
