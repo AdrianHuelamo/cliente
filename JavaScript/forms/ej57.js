04156c4461ec391c7dbba7204886272c47c374df
solvam <alumno@solvam.local>
2025-11-12 10:44:23 +0100

    12-11-solvam

--- JavaScript/forms/ej57.js
+++ JavaScript/forms/ej57.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         optionsI.value = "ireland";
         select.appendChild(optionsI);
 
+
         divdes.appendChild(select);
 
         let gender = document.getElementById("gender");
@@ -34,6 +35,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let myBtn = document.querySelector("input[type='button']");
     myBtn.addEventListener("click", () => {
-        
+        let formName = document.createElement("p");
+        let formPass = document.createElement("p");
+        let formAge = document.createElement("p");
+        let formInterests = document.createElement("p");
+        let formGender = document.createElement("p");
+        let formCountry = document.createElement("p");
+        let formComments = document.createElement("p");
+
+        let fName = document.getElementById("name").value;
+        let fPass = document.getElementById("password").value;
+        let fAge = document.getElementById("age").value;
+        let fInterests = document.getElementById("interests").value;
+        let fGender = document.getElementById("gender").value;
+        let fCountry = document.getElementById("country").value;
+        let fComments = document.getElementById("comments").value;
+
+        formName.textContent = fName;
+        formPass.textContent = fPass;
+        formAge.textContent = fAge;
+        formInterests.textContent = fInterests;
+        formGender.textContent = fGender;
+        formCountry.textContent = fCountry;
+        formComments.textContent = fComments;
+
+        document.appendChild(formName);
+        document.appendChild(formPass);
+        document.appendChild(formAge);
+        document.appendChild(formInterests);
+        document.appendChild(formGender);
+        document.appendChild(formCountry);
+        document.appendChild(formComments);
     });
 });
