c6bcb940cd07b5baba9abc9a4eecc838aa2ab0a8
solvam <alumno@solvam.local>
2025-12-05 09:51:12 +0100

    5-12-solvam

---
+++ JQuery/ej5.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //1
+    $("<li>Black Widow</li>").appendTo("ul")
+
+    //2
+    $("<li>Red Widow</li>").prependTo("ul")
+
+    //3
+
+    //4
+
+    //5
+
+})
