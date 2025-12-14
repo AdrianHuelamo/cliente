e916b4dba6dddab8df5cd7e1ae999a4cd378cc7e
adrian <adri41104@gmail.com>
2025-12-14 21:17:49 +0100

    14-12-casa

--- JQuery/ej21-30/ej21.js
+++ JQuery/ej21-30/ej21.js
@@ -29,8 +29,58 @@ $(document).ready(function() {
     //5
     $(".button:eq(4)").click(function(){
         $("img").animate({
-            up: '-100px',
-            rigth: '+100px',
+            top: '-=100%',
+            right: '+=100%',
         }, "fast");
+    });
+
+    //6
+    $(".button:eq(5)").click(function(){
+        $("img").animate({
+            width: '+=100px',
+        },2000, "linear");
+    });
+
+    //7
+    $(".button:eq(6)").click(function(){
+        $("img").animate({
+            opacity: '0',
+        },5000, "swing")  
+    });
+
+    //8
+    $(".button:eq(7)").click(function(){
+        $("img").animate({opacity: '0.5'},1000);
+        $("img").slideUp();
+    });
+
+    //9
+    $(".button:eq(8)").click(function(){
+        $("img").css('position', 'relative');
+        $("img").animate({width: '50%'}, "slow");
+        $("img").animate({top: '+=200px'}, "slow");
+        $("img").animate({left: '+=150px'}, "slow");
+        $("img").animate({top: '-=200px'}, "slow");
+        $("img").animate({left: '-=150px'}, "slow"); 
+    });
+
+    //10
+    $(".button:eq(9)").click(function(){
+        $("img").css('position', 'relative');
+        $("img").animate({
+            width: '200px',
+            height: '200px',
+        },2000, function(){
+            $("img").css("border-radius", '50%');
+            setTimeout(function() {
+            $("img").css("border-radius", "0");
+            }, 1000);
+        });
+        
+    });
+
+    //11
+    $(".button:eq(10)").click(function(){
+      $("img").stop(true, true).removeAttr('style');
     });
 })
