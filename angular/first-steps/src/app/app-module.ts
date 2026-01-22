dc429f294dfa121c00df9ff48584686e99312ac1
solvam <alumno@solvam.local>
2026-01-22 13:54:18 +0100

    22-1-solvam

---
+++ angular/first-steps/src/app/app-module.ts
@@ -0,0 +1,32 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Ads1 } from './components/ads1/ads1';
+import { Error } from './components/error/error';
+import { Footer } from './components/footer/footer';
+import { Home } from './components/home/home';
+import { Info } from './components/info/info';
+import { Navbar } from './components/navbar/navbar';
+
+@NgModule({
+  declarations: [
+    App,
+    Ads1,
+    Error,
+    Footer,
+    Home,
+    Info,
+    Navbar
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
