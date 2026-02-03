803dceaaec54491343b9136f20ddbe8177bb192b
adrian <adri41104@gmail.com>
2026-02-03 22:35:30 +0100

    3-2-casa

--- angular/first-steps/src/app/components/valorant/valorant.ts
+++ angular/first-steps/src/app/components/valorant/valorant.ts
@@ -10,7 +10,11 @@ import { Daum, InfoValorant } from '../../common/valorantinterface';
 })
 export class Valorant implements OnInit {
   dataApi!: InfoValorant
+
+  allCharacteres: Daum[] = [];
+  filteredCharacters: Daum[] = [];
   characters: Daum[] = [];
+
   activeIndex: number | null = null;
   charName: string = '';
   loading: boolean = false;
@@ -18,9 +22,9 @@ export class Valorant implements OnInit {
   currentPage: number = 1;
   totalPages: number = 0;
   pageToGo: number = 1;
-  allCharacteres: Daum[] = [];
   initialSlice!:number;
   finalSlice!: number;
+  sortState: number = 0;
 
   constructor(private valorantservices: Valorantservices) {
   }
@@ -34,8 +38,9 @@ export class Valorant implements OnInit {
     this.valorantservices.getCharacters().subscribe({
       next:(value) => {
         this.dataApi = value;
-        this.allCharacteres = this.dataApi.data;
-        this.totalPages =  Math.ceil( this.allCharacteres.length / this.elementsPP);
+        this.allCharacteres = [...this.dataApi.data];
+        this.filteredCharacters = [...this.dataApi.data];
+        this.calculateTotalPages();
         this.pagination();
         this.loading = false;
       },
@@ -49,32 +54,80 @@ export class Valorant implements OnInit {
     })
   }
 
+  calculateTotalPages() {
+     this.totalPages = Math.ceil(this.filteredCharacters.length / this.elementsPP);
+  }
+
   pagination() {
     this.initialSlice = (this.currentPage - 1) * this.elementsPP;
     this.finalSlice = this.initialSlice + this.elementsPP;
-    this.characters = this.allCharacteres.slice(this.initialSlice,this.finalSlice)
+    this.characters = this.filteredCharacters.slice(this.initialSlice,this.finalSlice)
   }
 
   toggleAccordion(index:number) {
     this.activeIndex = this.activeIndex === index ? null : index;
   }
 
+  //filterByName() {
+  //  this.filteredCharacters = this.allCharacteres.filter(char => char.displayName.toLocaleLowerCase().includes(this.charName.toLocaleLowerCase())); 
+  //  this.currentPage = 1;
+  //  this.calculateTotalPages();
+  //  this.pagination();
+  //}
+
+  applyFilterAndSort() {
+    this.filteredCharacters = this.allCharacteres.filter(char => 
+        char.displayName.toLocaleLowerCase().includes(this.charName.toLocaleLowerCase())
+    );
+
+    if (this.sortState === 1) {
+        this.filteredCharacters.sort((a, b) => 
+            a.displayName.toLocaleLowerCase().localeCompare(b.displayName.toLocaleLowerCase())
+        );
+    } else if (this.sortState === 2) {
+        this.filteredCharacters.sort((a, b) => 
+            b.displayName.toLocaleLowerCase().localeCompare(a.displayName.toLocaleLowerCase())
+        );
+    }
+
+    this.currentPage = 1;
+    this.calculateTotalPages();
+    this.pagination();
+  }
+
+  toggleSort() {
+    if (this.sortState === 0) {
+        this.sortState = 1; 
+    } else if (this.sortState === 1) {
+        this.sortState = 2; 
+    } else {
+        this.sortState = 0; 
+    }
+
+    // Aplicamos los cambios
+    this.applyFilterAndSort();
+  }
+
   filterByName() {
-    this.characters = this.characters.filter(char => char.displayName.toLocaleLowerCase().includes(this.charName.toLocaleLowerCase()));
+      this.applyFilterAndSort();
   }
 
   resetFilter() {
-    this.loadCharacters();
     this.charName = "";
+    this.filteredCharacters = [...this.allCharacteres];
+    this.calculateTotalPages();
+    this.currentPage = 1;
+    this.pagination();
   }
 
-  orderByName() {
-    this.characters.sort((a,b) => {
-      if (a.displayName.toLocaleLowerCase() > b.displayName.toLocaleLowerCase()) return 1;
-      else if (a.displayName.toLocaleLowerCase() < b.displayName.toLocaleLowerCase()) return -1;
-      else return 0;
-    });
-  }
+  //orderByName() {
+  //  this.filteredCharacters.sort((a,b) => {
+  //    if (a.displayName.toLocaleLowerCase() > b.displayName.toLocaleLowerCase()) return 1;
+  //    else if (a.displayName.toLocaleLowerCase() < b.displayName.toLocaleLowerCase()) return -1;
+  //    else return 0;
+  //  });
+  //  this.pagination();
+  //}
 
   nextPage() {
     if (this.currentPage < this.totalPages) {
@@ -91,9 +144,9 @@ export class Valorant implements OnInit {
   }
 
   goToPage(page: number) {
-    if (page < this.totalPages && page >= 1) {
+    if (page <= this.totalPages && page >= 1) {
       this.currentPage = page;
-      this.loadCharacters();
+      this.pagination();
     }
   }
 }
